69f14f563b0f42f1a6bcd9e583f3e95ea53e915e
Ruby122 <yiqing0412@gmail.com>
2017-10-21 16:53:33 -0400

    Add files via upload

---
+++ Code/Dependencies.py
@@ -0,0 +1,100 @@
+# Insert Yelp API Client ID and SECRET
+CLIENT_ID = 'CVmAoAtL2pFNiBp4C3pdyQ'
+CLIENT_SECRET = 'cSmEfzCv1wVvuQQUUHtsHXpN5R8a2QGnPIl2ZcbrgQinHs1F2dX3fQVLslMQFpND'
+
+# Set File Path/Name for resulting data set
+# remeber, use '\\' instead of '\' to escape \
+yelp_general_outfile_path = '\\Users\\Ruby\\OSU\\2017Autumn\\BusinessCluster\\Team11Yelp-master\\Scripts\\YelpDatatest.csv'
+
+reviews_outfile_path = '\\Users\\Ruby\\OSU\\2017Autumn\\BusinessCluster\\Team11Yelp-master\\Scripts\\YelpReviewtest.csv'
+
+# Found on https://www.yelp.com/developers/documentation/v2/all_category_list
+# Set list of Category aliases desired
+
+
+alias_list = ['asianfusion',
+              'african',
+              'newamerican',
+              'tradamerican',
+              'argentine',
+              'bbq',
+              'brazilian',
+              'breakfast_brunch',
+              'british',
+              'buffets',
+              'burgers',
+              'caribbean',
+              'chicken_wings',
+              'chinese',
+              'cuban',
+              'diners',
+              'hotdogs',
+              'filipino',
+              'french',
+              'fishnchips',
+              'german',
+              'gluten_free',
+              'greek',
+              'halal',
+              'hawaiian',
+              'hotdog',
+              'hotpot',
+              'indpak',
+              'irish',
+              'italian',
+              'japanese',
+              'korean',
+              'latin',
+              'mediterranean',
+              'mexican',
+              'mideastern',
+              'mongolian',
+              'noodles',
+              'pakistani',
+              'pizza',
+              'polish',
+              'russian',
+              'salad',
+              'sandwiches',
+              'seafood',
+              'soulfood',
+              'spanish',
+              'steakhouse',
+              'sushi',
+              'tapas',
+              'tex-mex',
+              'thai',
+              'turkish',
+              'vegan',
+              'vegetarian',
+              'vietnamese',
+              'waffles']
+#Asian: asianfusion, chinese, filipino, halal,hawaiian, hotpot, indpak, japanese, korean, mideastern, mongolian, noodles
+#pakistani, russian,sushi,thai,vietnamese,
+#African: african, mediterranean,
+#American: newamerican, tradamerican, burgers, chicken_wings, hotdogs, hotdog,pizza,salad, sandwiches,soulfood,
+#Latin: argentinem, brazilian, caribbean, cuban, latin,mexican,tex-mex,
+#European: british,french,fishnchips, german, greek,irish, italian,polish,spanish,steakhouse,tapas,turkish, waffles
+
+
+# No arabian, austrian, bistros, dumplings, indonesian, jewish, kebab, kosher, malaysian, meatballs, moroccan, persian,
+# No portuguese, scandinavian, singaporean, syrian, taiwanese, wraps
+
+# Food categories that we have in Columbus
+alias_list2 = ['bbq',
+               'burgers',
+               'buffets',
+               'fishnchips',
+               'hotdogs',
+               'hotpot',
+               'hotdog',
+               'noodles',
+               'pizza',
+               'indpak',
+               'salad',
+               'sushi',
+               'sandwiches',
+               'chicken_wings',
+               'waffles',]
+
+
